223b90d71a64339bcb858d1e742cd8da7a345a29
mvoisin64 <mvoisin92@gmail.com>
2025-08-14 14:41:21 +0200

    project page first draft

--- src/Components/ProjectPage/ProjectPage.jsx
+++ src/Components/ProjectPage/ProjectPage.jsx
@@ -24,13 +24,18 @@ function ProjectPage() {
     }
 
 
-
-
     return (
         <main className='relative min-h-screen overflow-x-hidden'>
             <div className='absolute -top-28 -left-28 w-[500px] h-[1000px] bg-gradient-to-tr from-emerald-400/40 to-pink-500/20 rounded-full blur-[50px] -z-10'></div>
             <div className='overflow-hidden px-11'>
 
+                <div>
+                    <img src={project.cover} alt="cover project" className=" object-contain w-120 h-120 rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300" />
+                </div>
+                <div>
+                    <h2 className='text-1xl mb-4 text-center font-bold pb-14'>{project.description}</h2>
+                    <p className='text-pink-300 text-center font-bold'>{project.technologies}</p>
+                </div>
 
 
 
