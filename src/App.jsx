1aaeadaaf313548a42ea6808bdc1de5e94f3dc12
mvoisin64 <mvoisin92@gmail.com>
2025-08-22 16:06:52 +0200

    modification import Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProjectPage from './Components/ProjectPage/ProjectPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage.jsx'
 import NotFound from './Components/NotFound.jsx'
-
+import {BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
