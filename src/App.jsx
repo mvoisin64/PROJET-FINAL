c13d7cf24bb7417a094ba7b8ba8cbe8ea1d953c4
mvoisin64 <mvoisin92@gmail.com>
2025-08-22 16:21:47 +0200

    correction imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 import ProjectPage from './Components/ProjectPage/ProjectPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage.jsx'
 import NotFound from './Components/NotFound.jsx'
-import {Routes, Route } from "react-router";
+import {Routes, Route } from "react-router-dom";
 
 function App() {
   return (
